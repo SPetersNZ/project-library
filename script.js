b02ae45a99b8897aaf4878729c167327a0497934
Steven Peters <137010917+SPetersNZ@users.noreply.github.com>
2025-01-31 17:44:34 +1300

    Page will now display title of added book

--- script.js
+++ script.js
@@ -8,13 +8,31 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-inputButton.addEventListener("click", addBookToLibrary);
-
-function addBookToLibrary() {
+function getInputValues () {
     const inputTitle = document.getElementById('title').value;
     const inputAuthor = document.getElementById('author').value;
     const inputPages = document.getElementById('pages').value;
     const inputRead = document.querySelector('input[name="read"]:checked').value;
+    return { inputTitle, inputAuthor, inputPages, inputRead};
+}
+
+inputButton.addEventListener("click", addBookToLibrary);
+
+function addBookToLibrary() {
+    
+    const { inputTitle, inputAuthor, inputPages, inputRead} = getInputValues();
+
     const newBook = new Book(inputTitle, inputAuthor, inputPages, inputRead);
     myLibrary.push(newBook);
+    displayBooks();
+}
+
+function displayBooks() {
+    
+    const { inputTitle, inputAuthor, inputPages, inputRead} = getInputValues();
+
+    const displayContainer = document.getElementById('display-container');
+    const titleDiv = document.createElement('div');
+    titleDiv.textContent += inputTitle;
+    displayContainer.appendChild(titleDiv);
 }
